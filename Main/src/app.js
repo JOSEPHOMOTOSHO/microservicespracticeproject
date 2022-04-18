bc797ba606eeab00f91b5495fba75b94e0b183f4
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-04-18 19:01:48 +0100

    env update

--- Main/src/app.js
+++ Main/src/app.js
@@ -1,15 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+require('dotenv').config();
 var express = require("express");
 // import { Request, Response } from "express";
 var cors = require("cors");
 var typeorm_1 = require("typeorm");
-// import { Product } from "./entity/product";
+var product_1 = require("./entity/product");
 var morgan = require("morgan");
 var amqp = require("amqplib/callback_api");
 (0, typeorm_1.createConnection)()
     .then(function (db) {
-    amqp.connect("amqps://hcmesmrl:OLXTRqJhp_o4_FMaSfc_NhHyvXZ4gHa5@cow.rmq2.cloudamqp.com/hcmesmrl", function (error0, connection) {
+    var productRepository = db.getMongoRepository(product_1.Product);
+    amqp.connect(process.env.AMQP_URI, function (error0, connection) {
         if (error0) {
             throw error0;
         }
@@ -17,15 +20,41 @@ var amqp = require("amqplib/callback_api");
             if (error1) {
                 throw error1;
             }
+            channel.assertQueue("product_created", { durable: false });
+            channel.assertQueue("product_deleted", { durable: false });
+            channel.assertQueue("product_updated", { durable: false });
+            var app = express();
+            app.use(cors({
+                origin: ["http://localhost:3000"],
+            }));
+            app.use(morgan("combined"));
+            app.use(express.json());
+            channel.consume("product_created", function (message) { return tslib_1.__awaiter(void 0, void 0, void 0, function () {
+                var eventProduct, product;
+                return tslib_1.__generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            eventProduct = JSON.parse(message.content.toString());
+                            product = new product_1.Product();
+                            product.admin_id = parseInt(eventProduct.id);
+                            product.title = eventProduct.title;
+                            product.likes = eventProduct.likes;
+                            product.image = eventProduct.image;
+                            return [4 /*yield*/, productRepository.save(product)];
+                        case 1:
+                            _a.sent();
+                            console.log("product created");
+                            return [2 /*return*/];
+                    }
+                });
+            }); }, { noAck: true });
+            console.log("Main app listening to port:8001");
+            app.listen(8001);
+            process.on("beforeExit", function () {
+                console.log("Closing connection");
+                connection.close();
+            });
         });
     });
-    var app = express();
-    app.use(cors({
-        origin: ["http://localhost:3000"],
-    }));
-    app.use(morgan("combined"));
-    app.use(express.json());
-    console.log("app listening to port:8001");
-    app.listen(8001);
 })
     .catch(function (err) { return console.log(err); });
--- Main/src/app.ts
+++ Main/src/app.ts
@@ -1,15 +1,18 @@
+require('dotenv').config()
 import * as express from "express";
 // import { Request, Response } from "express";
 import * as cors from "cors";
 import { createConnection } from "typeorm";
-// import { Product } from "./entity/product";
+import { Product } from "./entity/product";
 import * as morgan from "morgan";
 import * as amqp from "amqplib/callback_api"
 
 createConnection()
   .then((db) => {
 
-    amqp.connect("amqps://hcmesmrl:OLXTRqJhp_o4_FMaSfc_NhHyvXZ4gHa5@cow.rmq2.cloudamqp.com/hcmesmrl",(error0,connection)=>{
+    const productRepository = db.getMongoRepository(Product)
+
+    amqp.connect(process.env.AMQP_URI as string,(error0,connection)=>{
       if(error0){
         throw error0
       }
@@ -17,20 +20,40 @@ createConnection()
         if(error1){
           throw error1
         }
+        channel.assertQueue("product_created", {durable:false})
+        channel.assertQueue("product_deleted", {durable:false})
+        channel.assertQueue("product_updated", {durable:false})
+        const app = express();
+        app.use(
+          cors({
+            origin: ["http://localhost:3000"],
+          })
+        );
+    
+        app.use(morgan("combined"));
+        app.use(express.json());
+
+        channel.consume("product_created", async (message)=>{
+          const eventProduct:Product = JSON.parse(message!.content.toString())
+          const product = new Product()
+          product.admin_id = parseInt(eventProduct.id)
+          product.title = eventProduct.title
+          product.likes = eventProduct.likes
+          product.image = eventProduct.image
+          await productRepository.save(product)
+          console.log("product created")
+        },{noAck:true})
+    
+        console.log("Main app listening to port:8001");
+    
+        app.listen(8001);
+        process.on("beforeExit", ()=>{
+          console.log("Closing connection")
+          connection.close()
+        })
       })
+
     })
-    const app = express();
-    app.use(
-      cors({
-        origin: ["http://localhost:3000"],
-      })
-    );
 
-    app.use(morgan("combined"));
-    app.use(express.json());
-
-    console.log("app listening to port:8001");
-
-    app.listen(8001);
   })
   .catch((err) => console.log(err));
